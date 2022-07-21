446574b3998ae19b729b1c839b3b1b498f1bdfd4
Alpin Gencer <alpingencer@gmail.com>
2022-07-21 09:29:00 +0200

    Add PHPUnit test structure

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * The Eufony ORM Package
+ * Copyright (c) 2022 Alpin Gencer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+require __DIR__ . "/../vendor/autoload.php";
+
+echo "\n";
+echo "Testsuite for the Eufony ORM by Alpin Gencer and contributors.\n";
+echo "\n";
